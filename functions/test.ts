c550081ae20805f891e93eb991d5ea8fc32f8454
Nicolás Georger <info@sredevops.dev>
2023-02-13 01:52:58 -0300

    Create test.ts
    Signed-off-by: Nicolás Georger <info@sredevops.dev>

---
+++ functions/test.ts
@@ -0,0 +1,13 @@
+import { Context } from "https://edge.netlify.com";
+
+export default async (request: Request, context: Context) => {
+
+  const joke = await fetch("https://icanhazdadjoke.com/", {
+    "headers": {
+      "Accept": "application/json"
+    }
+  });
+  
+  const jsonData = await joke.json();
+  return Response.json(jsonData);
+};
